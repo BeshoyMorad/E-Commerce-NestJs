aec540f2a5423b788aedb5fb7a4318815b42fd2f
BeshoyMorad <besh0morta@gmail.com>
2024-01-24 09:54:47 +0200

    Intialize the project and connect to database

---
+++ db/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+config();
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [],
+  migrations: [],
+  logging: false,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
