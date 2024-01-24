8ecd69db4222db1f16bc49770aa62f7dc875fa09
BeshoyMorad <besh0morta@gmail.com>
2024-01-24 10:27:40 +0200

    Add database migrations

--- db/data-source.ts
+++ db/data-source.ts
@@ -10,9 +10,9 @@ export const dataSourceOptions: DataSourceOptions = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations: [],
+  migrations: ['dist/db/migrations/*{.ts,.js}'],
   logging: false,
-  synchronize: true,
+  synchronize: false,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
